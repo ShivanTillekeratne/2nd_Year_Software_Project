06e11cc8e061c4b6812532881ebca8ce58d50e2c
unknown <shivanabi123@gmail.com>
2024-04-05 00:02:40 +0530

    creating a model

--- Backend_Creatio/health_insurance/models.py
+++ Backend_Creatio/health_insurance/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    name = models.CharField(max_Length=200)
+    description = models.CharField(max_Length=500)
