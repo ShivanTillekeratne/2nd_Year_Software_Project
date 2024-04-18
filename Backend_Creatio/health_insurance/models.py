d3d6e52db11f37df99e721be98000ebb942fbaf7
unknown <shivanabi123@gmail.com>
2024-04-18 22:13:54 +0530

    creating API with Swagger UI in django

--- Backend_Creatio/health_insurance/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Data(models.Model):
-    name = models.CharField(max_Length=200)
-    description = models.CharField(max_Length=500)
