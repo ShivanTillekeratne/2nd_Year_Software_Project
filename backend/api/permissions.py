50deab38890dd838eeeee12929192b8bb83c8854
Supankan <91975692+Supankan@users.noreply.github.com>
2024-07-07 11:19:35 +0530

    Delete backend directory

--- backend/api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-from .models import CustomUser
-
-class IsRoleAdmin(permissions.BasePermission):
-    """
-    Custom permission to only allow users with the 'Admin' role to access certain views.
-    """
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role == CustomUser.Role.ADMIN.value
