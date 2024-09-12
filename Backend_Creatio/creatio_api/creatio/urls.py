e420c28aa6b22e182cb223d737acab5bca252b4a
Shivan Tillekeratne <77282555+ShivanTillekeratne@users.noreply.github.com>
2024-09-13 00:03:51 +0530

    Delete Backend_Creatio/creatio_api directory

--- Backend_Creatio/creatio_api/creatio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from creatio import views
-urlpatterns = [
-    path('claims/<int:claim_id>/', views.GetClaimDetails.as_view(), name='get-claim-details'),
-    path('claims/<int:claim_id>/check_fraud/', views.CheckFraud.as_view(), name='check-fraud'),
-     path('customers/<int:customer_id>/claims/', views.GetCustomerClaims.as_view(), name='get-customer-claims'),
-     path('fraudulent_claims/', views.FraudulentClaimList.as_view(), name='fraudulent-claims'),
-]
